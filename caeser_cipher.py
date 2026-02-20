c50fe7ca75c1cab4d02e2abf51f862c4f213372a
Shrutika Vasant Pawar <shrutika883@gmail.com>
2026-02-20 11:03:11 +0530

    The caeser_cipher.py file is added and few baisc validation is added, yet not complete on's ...

---
+++ caeser_cipher.py
@@ -0,0 +1,29 @@
+
+def normalize_shift(shift):
+    if not isinstance(shift, int):
+        raise TypeError("Shift must be integer !!!!");
+    return shift % 26     #handles negative & large shifts
+
+def caeser_cipher(text, shift_value):
+    if not isinstance(text, str):
+        raise TypeError ("Text must be string")
+    shift_value = normalize_shift(shift_value)
+       
+
+    result =[]
+    for char in text:
+        if char.isupper():
+            result.append( chr((ord(char) -65 + shift_value) % 26 + 65))
+        elif char.islower():
+            result.append( chr((ord(char) -97 + shift_value) % 26 + 97))
+        else :
+            result.append(char)
+    return "".join(result)    #list + join is used for faster memory usage
+
+def caeser_decrypt (text, shift_value):
+    shift_value = normalize_shift(shift_value)
+    return caeser_cipher(text, - shift_value)
+
+#Example
+text = "Hello World !!!!!"
+encrypted = caeser_cipher(text, 3)
