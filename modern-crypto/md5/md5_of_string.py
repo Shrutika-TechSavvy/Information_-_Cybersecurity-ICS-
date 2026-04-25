d813cd7b430b04bb548817952775601b87d95bf8
Shrutika Pawar <shrutikapawar951@gmail.com>
2026-04-25 18:56:44 +0530

    The mdf_of_string is updated..

--- modern-crypto/md5/md5_of_string.py
+++ modern-crypto/md5/md5_of_string.py
@@ -1,7 +1,17 @@
 import hashlib
+def generate_md5(text, encoding = "utf-8", errors = "strict"):
+    #Validation : type check
+    if not isinstance(text, str):
+        raise TypeError("Input must be of string...")
+    #Validation : empty string
+    if text == "":
+        raise ValueError("Input string cannot be empty...")
+    
+    try:
+        byte_data = text.encode(encoding, errors = errors)
+    except UnicodeEncodeError as e:
+        print("Encoding error : ", e)
 
-text = "Hello world !!!"
-#Encode string t bytes , then hash
-md5_hash = hashlib.md5(text.encode())
-#Get the hexadecimal representation
-print("md5 hex representationn:", md5_hash.hexdigest()) 
+    md5 = hashlib.md5()
+    md5.update(byte_data)
+    return md5.hexdigest()
